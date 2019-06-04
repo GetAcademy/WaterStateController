51ae9dc1bdc71f2275f8b985bd349c8fb7be0a31
GET Academy <terje@kolderup.net>
2019-06-04 12:43:47 +0200

    x

--- WaterStateController/Water.cs
+++ WaterStateController/Water.cs
@@ -2,6 +2,13 @@
 {
     public class Water
     {
+        public Water(int gram, int temperature, int proportion = 100)
+        {
+            
+        }
+
+        public WaterState State { get; }
+        public int Temperature { get; }
     }
 
     public enum WaterState
--- WaterStateControllerTest/WaterStateControllerTest.cs
+++ WaterStateControllerTest/WaterStateControllerTest.cs
@@ -8,10 +8,37 @@ namespace WaterStateControllerTest
     public class WaterStateControllerTest
     {
         [TestMethod]
-        public void Test1()
+        public void TestWaterAt20Degrees()
         {
-            var water = new Water();
+            var water = new Water(50, 20);
             Assert.AreEqual(WaterState.Fluid, water.State);
+            Assert.AreEqual(20, water.Temperature);
         }
+
+        [TestMethod]
+        public void TestIceAtMinus20Degrees()
+        {
+            var water = new Water(50, -20);
+            Assert.AreEqual(WaterState.Ice, water.State);
+            Assert.AreEqual(-20, water.Temperature);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+            "When temperature is 0 or 100, you must provide a value for proportion")]
+        public void TestWaterAt100DegreesWithoutProportion()
+        {
+            var water = new Water(50, 100);
+        }
+
+        [TestMethod]
+        public void TestWaterAt100Degrees()
+        {
+            var water = new Water(50, 100, 30);
+            Assert.AreEqual(WaterState.FluidAndGas, water.State);
+            Assert.AreEqual(20, water.Temperature);
+        }
+
+        
     }
 }
